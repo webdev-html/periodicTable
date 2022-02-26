4fc7a4ec26f617dfa64cafad2f77ea32b3c1189f
webdev-html <73722228+webdev-html@users.noreply.github.com>
2022-02-26 09:31:34 -0500

    Create Groups.py
    This file is for finding elements in the same groups as each other, the main file also imports this file for that reason

---
+++ Groups.py
@@ -0,0 +1,33 @@
+groups = {'Reactive Nonmetals':"""Hydrogen,Carbon,Nitrogen,Oxygen,Fluorine,
+Phosphorus,Sulfer,Chlorine,Selenium,Bromine,Iodine""",
+
+'Metalloids':"""Boron,Silicon,Germanium,Arsenic,
+Antimony,Tellurium,Astatine""",
+
+'Post Transition Metals':"""Aluminum,Gallium,Indium,Tin,
+Thallium,Lead,Bismuth,Polonium""",
+
+"Transition Metals":"""Scandium,Titanium,Vanadium,Chromium,Manganese,Iron,
+Cobalt,Nickel,Copper,Zinc,
+Yttrium,Zirconium,Niobium,Molybdenum,Technetium,
+Ruthenium,Rhodium,Palladium,Silver,Cadmium,Hafnium,
+Tantalum,Tungsten,Rhenium,Osmium,
+Iridium,Platinum,Gold,Mercury,Rutherfordium,
+Dubnium,Seaborgium,Bohrium,Hassium""",
+
+'Noble Gases':'Helium,Neon,Argon,Krypton,Xenon,Radon',
+
+'Lanthanoids':"""Lanthanum,Cerium,Praseodymium,Neodymium,Promethium,
+Samarium,Europium,Gadolinium,Terbium,
+Dysprosium,Holmium,Erbium,Thulium,Ytterbium,Lutetium""",
+
+'Actinoids':"""Actinium,Thorium,Protactinium,
+Uranium,Neptunium,Plutonium,
+Americium,Curium,Berkelium,Californium,Einsteinium,Fermium,
+Mendelevium,Nobelium,Lawencium""",
+
+'Alkali Metals':'Lithium,Sodium,Potassium,Rubidium,Caesium,Francium',
+
+'Alkaline Earth Metals':'Beryllium,Magnesium,Calcium,Strontium,Barium,Radium',
+
+'None':"Meitnerium,Damstadium,Roentgenium,Copemicium,Nihonium,Flerovium,Moscovium,Livermorium,Temmessine,Oganesson"}
