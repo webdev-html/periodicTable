594d941686fa03ae0d42a7ccfc8b6e23c6f53a52
webdev-html <73722228+webdev-html@users.noreply.github.com>
2022-03-12 08:19:06 -0500

    Groupdefs

--- Groups.py
+++ Groups.py
@@ -32,3 +32,15 @@ Mendelevium,Nobelium,Lawencium""",
 'Alkaline Earth Metals':'Beryllium,Magnesium,Calcium,Strontium,Barium,Radium',
 
 'None':"Meitnerium,Damstadium,Roentgenium,Copemicium,Nihonium,Flerovium,Moscovium,Livermorium,Temmessine,Oganesson"}
+
+groupdefs = {
+'Reactive Nonmetals':'The Reactive Nonmetals are a group on the periodic table that are less dense then metals,and do not reflect any light(except for diamond)',
+'Noble Gases':'The Noble Gases are a group of gases on the periodic table. these gases are mainly catagorized by the fact that they are inert, meaning they can not react with other elements',
+'Alkali Metals':'The Alkali Metals are a group of metals that melt at very low temperatures,are amazing at thermal and eletrical conductivity,and are extremely reactive with other metals',
+'Alkaline Earth Metals':'Alkaline Earth Metals lose electrons very easily,which is why most of their occurences are ionic.The Alkaline Earth Metals are also metallic and are good eletrical conducters',
+'Transition Metals':'The Transition Metals have the most amount of elements in them,and they actually have 2 electron shells instead of one.',
+'Post Transition Metals':'The Post Transition Metals serve as a barrior between the metals and the non metals.they have very high density,and are very brittle',
+'Lanthanoids':'The Lanthanoids are a group of rare earth metals that are prevelent around the world,and a part of the inner transition metal family',
+'Actinoids':'The Actinoids are a purely radioactive group,in which only uranium,protactium,and thorium are the only non man made elements.The Actinoids are primarily used in fission reactions because of this intense radioactivity  ',
+'Metalloids':'The Metalloids Share properties of metals and non metals,such as how they are shiny,but also brittle.The Metalloids are also decent conducters ',
+None:'These Elements Do not have a group,as these elements only existed for seconds on earth before turning into different elements,because of their radioactive decay'}
